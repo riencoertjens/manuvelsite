9da6e6d9598107610d6ca845c2d3a4498a8db2bd
Rien Coertjens <rien.coertjens@gmail.com>
2019-11-03 18:37:37 +0700

    Revert "feat: set autumn break"
    This reverts commit 0eabfe1e9429067ac1ec8b11a63c522e47ba1f7f.

--- src/pages/index.js
+++ src/pages/index.js
@@ -338,7 +338,16 @@ const IndexPage = ({ data }) => {
               <h2>contact</h2>
               <table>
                 <tr>
-                  <td colSpan={4}>closed: Oct 29 - Nov 4</td>
+                  <td>Tue-Fri</td>
+                  <td>8h - 18h</td>
+                  <td>breakfast</td>
+                  <td>9h - 10h30</td>
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>9h - 17h</td>
+                  <td>lunch</td>
+                  <td>12h - 14h</td>
                 </tr>
                 <tr
                   css={css`
@@ -355,11 +364,9 @@ const IndexPage = ({ data }) => {
                   <td colSpan={4} />
                 </tr>
                 <tr>
-                  <td colSpan={4}>
-                    Manuvèl heeft een herfstbreak van 29 oktober tot en met 4
-                    november om ons de tijd te geven met nieuwe frisse ideeën op
-                    de proppen te komen
-                  </td>
+                  <td>Sunday</td>
+                  <td>9h - 13h</td>
+                  <td colSpan={2}>lazy breakfast until 12h</td>
                 </tr>
                 <tr
                   css={css`
@@ -375,7 +382,7 @@ const IndexPage = ({ data }) => {
                 >
                   <td colSpan={4} />
                 </tr>
-                {/* <tr>
+                <tr>
                   <td>Monday</td>
                   <td>rest day</td>
                   <td colSpan={2}>
@@ -398,7 +405,7 @@ const IndexPage = ({ data }) => {
                   `}
                 >
                   <td colSpan={2} />
-                </tr> */}
+                </tr>
               </table>
 
               <div
